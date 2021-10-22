6879eaec31664b2aa7f28c91c297f5213d047663
Bluenix <bluenixdev@gmail.com>
2021-10-23 00:57:17 +0200

    Add dead-simple setup.py

---
+++ setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+
+setup(
+    name='discord_gateway',
+    version='0.0.1',
+    description='Sans-I/O implementation of the Discord gateway.',
+    install_requires=['wsproto'],
+    extras_require={'etf': ['erlpack'], 'perf': ['ujson']},
+)
