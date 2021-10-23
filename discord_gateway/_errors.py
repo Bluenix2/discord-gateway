3ef7e76b0dbef05dd3d42bd2d8f6d2a954dc5a23
Bluenix <bluenixdev@gmail.com>
2021-10-23 20:24:07 +0200

    Make the initial separation into multiple files

---
+++ discord_gateway/_errors.py
@@ -0,0 +1,28 @@
+from wsproto.events import RejectConnection
+
+__all__ = ('CloseDiscordConnection', 'ConnectionRejected')
+
+
+class CloseDiscordConnection(Exception):
+    """Signalling exception notifying the socket should be closed.
+
+    Contained in this exception is some last bytes to send off before closing
+    the WebSocket.
+    """
+
+    def __init__(self, data: bytes) -> None:
+        super().__init__()
+
+        self.data = data
+
+
+class ConnectionRejected(Exception):
+    """Exception raised when the connection to Discord was rejected."""
+
+    def __init__(self, event: RejectConnection) -> None:
+        super().__init__(
+            f'Discord rejected the WebSocket connection - Error code {event.status_code}'
+        )
+
+        self.code = event.status_code
+        self.headers = event.headers
